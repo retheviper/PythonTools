8f19eb4bee80c5be0926ec0d5d238ae66c04deb8
retheviper <youngbina@hotmail.com>
2020-09-04 20:32:18 +0900

    [fix] little fix

--- ExtensionHandler/extention_handler.py
+++ ExtensionHandler/extention_handler.py
@@ -15,17 +15,21 @@ print('Base path: ' + os.path.abspath(walk_dir))
 for root, subdirs, files in os.walk(walk_dir):
 
     for filename in files:
+
+        if filename.startswith('._'):
+            continue
         
         file_path = os.path.join(root, filename)
         guess_mime_type = mimetypes.guess_type(file_path)[0]
+
+        if guess_mime_type is None or 'text' in guess_mime_type:
+            continue
+
         actual_mime_type = str(magic.from_file(file_path, mime=True))
         file_name_splited = os.path.splitext(file_path)
         extension = file_name_splited[1]
 
-        if 'text' in guess_mime_type:
-            continue
-
-        if (guess_mime_type != actual_mime_type) & ('application' not in actual_mime_type):
+        if guess_mime_type != actual_mime_type and 'application' not in actual_mime_type:
             print('Change {0}\'s extention because it looks like {1} but actually {2}'.format(filename, guess_mime_type, actual_mime_type))
             os.rename(file_path, file_name_splited[0] + mimetypes.guess_extension(actual_mime_type, True))
 
