762516b1d9ba9bbc5814b2eb228138dcd9c1d073
retheviper <youngbina@hotmail.com>
2020-09-25 10:26:18 +0900

    [add] git puller

---
+++ GitPuller/README.md
@@ -0,0 +1,10 @@
+# Git Puller
+
+## TL;DR
+
+Pull current working branch if the directory has `.git`. (Recursive)
+
+## To Use
+
+2. Install gitpython (ex: `pip install -r requirements.txt`)
+1. run `python3 git_puller.py /dir`
---
+++ GitPuller/git_puller.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import git
+
+if len(sys.argv) < 2:
+    print('Need argument of base path. quitting.')
+    sys.exit(1)
+
+walk_dir = sys.argv[1]
+
+print('Base path: ' + os.path.abspath(walk_dir))
+
+for root, subdirs, files in os.walk(walk_dir):
+    
+    if '.git' in subdirs:
+        repo = git.Repo(root)
+        branch = repo.active_branch
+        print('Pulling [{}]'.format(root))
+        os.system('git pull origin {}'.format(branch.name))
