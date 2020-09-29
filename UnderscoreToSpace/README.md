489a5f8a3b0fe197de52acc314d974c40233cb0e
retheviper <youngbina@hotmail.com>
2020-09-29 11:44:15 +0900

    [add] underscore to space

---
+++ UnderscoreToSpace/README.md
@@ -0,0 +1,9 @@
+# Underscore to space
+
+## TL;DR
+
+Rename file's name with underscore to space. (Recursive)
+
+## To Use
+
+1. run `python3 underscore_to_space.py /dir`
---
+++ UnderscoreToSpace/underscore_to_space.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import filecmp
+
+if len(sys.argv) < 2:
+    print('Need argument of base path. quitting.')
+    sys.exit(1)
+
+walk_dir = sys.argv[1]
+
+print('Base path: ' + os.path.abspath(walk_dir))
+
+for root, subdirs, files in os.walk(walk_dir):
+
+    for filename in files:
+
+        if filename.startswith('.'):
+            continue
+
+        if '_' in filename:
+            file_path = os.path.join(root, filename)
+            file_renamed_path = os.path.join(root, filename.replace('_', ' '))
+            print('Changing {} to {}.'.format(file_path, file_renamed_path))
+            os.rename(file_path, file_renamed_path)
