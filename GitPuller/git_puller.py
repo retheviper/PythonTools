98ad8d3fd373c611038d2aa512561904fadbd00e
retheviper <youngbina@hotmail.com>
2020-10-08 22:39:59 +0900

    [fix] bug

--- GitPuller/git_puller.py
+++ GitPuller/git_puller.py
@@ -6,15 +6,15 @@ if len(sys.argv) < 2:
     print('Need argument of base path. quitting.')
     sys.exit(1)
 
-walk_dir = sys.argv[1]
+walk_dir = os.path.abspath(sys.argv[1])
 
-print('Base path: ' + os.path.abspath(walk_dir))
+print('Base path: ' + walk_dir)
 
 for root, subdirs, files in os.walk(walk_dir):
     
     if '.git' in subdirs:
-        repo = git.Repo(root)
-        branch = repo.active_branch
+        branch = git.Repo(root).active_branch.name
         print('Pulling [{}]'.format(root))
         os.chdir(root)
-        os.system('git pull origin {}'.format(branch.name))
+        os.system('git pull origin {}'.format(branch))
+        subdirs[:] = []
