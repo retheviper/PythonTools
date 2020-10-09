1af04d5274059986d0564cb246560bb72403ff6b
retheviper <youngbina@hotmail.com>
2020-10-09 23:00:30 +0900

    [fix] make more sense with variable name

--- DuplicateRemover/duplicate_remover.py
+++ DuplicateRemover/duplicate_remover.py
@@ -10,23 +10,24 @@ walk_dir = sys.argv[1]
 
 print('Base path: ' + os.path.abspath(walk_dir))
 
-for root, subdirs, files in os.walk(walk_dir):
+for root, subdirs, original_files in os.walk(walk_dir):
     
-    sub_files = files.copy()
+    target_files = original_files.copy()
 
-    for filename in files:    
+    for original_file in original_files:    
     
-        for filename2 in sub_files:
+        for target_file in target_files:
     
-            file_path = os.path.join(root, filename)
-            file2_path = os.path.join(root, filename2)
+            original_file_path = os.path.join(root, original_file)
+            target_file_path = os.path.join(root, target_file)
     
-            if filename is not filename2 and filecmp.cmp(file_path, file2_path):
+            if original_file is not target_file and filecmp.cmp(original_file_path, target_file_path):
     
-                long_duplicate = file_path if len(filename) > len(filename2) else file2_path
+                longer_file_name = original_file_path if len(original_file) > len(target_file) else target_file_path
+                
                 try:
-                    sub_files.remove(os.path.split(long_duplicate)[1])
-                    os.remove(long_duplicate)
-                    print("'{}' and '{}' is same file. deleted '{}'.".format(file_path, file2_path, long_duplicate))
+                    target_files.remove(os.path.split(longer_file_name)[1])
+                    os.remove(longer_file_name)
+                    print("'{}' and '{}' is same file. deleted '{}'.".format(original_file_path, target_file_path, longer_file_name))
                 except:
                     continue
