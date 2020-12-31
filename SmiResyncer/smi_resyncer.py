51d6cb2ae992e59e15a6d4e5d4b1b90036e98a5e
retheviper <youngbina@hotmail.com>
2020-12-31 13:18:34 +0900

    fix: autodetect encoding

--- SmiResyncer/smi_resyncer.py
+++ SmiResyncer/smi_resyncer.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import codecs
+import magic
 
 if len(sys.argv) < 3:
     print('Need argument of base path. quitting.')
@@ -8,12 +9,12 @@ if len(sys.argv) < 3:
 
 file_path = os.path.abspath(sys.argv[1])
 milsec = int(float(sys.argv[2]) * 1000)
-utf8 = 'utf-8'
 ignore = 'ignore'
-
-with codecs.open(file_path, 'r', encoding=utf8, errors=ignore) as file:
+f = magic.Magic(uncompress = True, mime_encoding = True)
+encoding = f.from_file(file_path)
+with codecs.open(file_path, 'r', encoding = encoding, errors = ignore) as file:
     lines = file.readlines()
-with codecs.open(file_path, 'w', encoding=utf8, errors=ignore) as file:
+with codecs.open(file_path, 'w', encoding = encoding, errors = ignore) as file:
     for line in lines:
         alter_line = line.lower()
         if '<sync start' in alter_line:
