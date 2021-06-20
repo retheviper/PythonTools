ec86f42deaedc2401829d6e4fe702e1d01e65eec
retheviper <retheviper@gmail.com>
2021-06-21 00:05:11 +0900

    fix bug

--- SmiResyncer/smi_resyncer.py
+++ SmiResyncer/smi_resyncer.py
@@ -18,7 +18,9 @@ with codecs.open(file_path, 'w', encoding = encoding, errors = ignore) as file:
     for line in lines:
         alter_line = line.lower()
         if '<sync start' in alter_line:
-            original = alter_line[alter_line.find('=') + 1 : alter_line.find('>')]
+            start_index = alter_line.find('=') + 1
+            end_index = alter_line.find('end') - 1 if alter_line.find('end') > 0 else alter_line.find('>')
+            original = alter_line[start_index : end_index]
             replaced = str(int(original) + milsec)
             line = line.replace(original, replaced)
         file.write(line)
