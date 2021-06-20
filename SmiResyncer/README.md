cc1d9a6c2f4d5ab7fa6791cd7c2710e3fa2f38cb
retheviper <retheviper@gmail.com>
2021-06-21 00:27:14 +0900

    fix

--- SmiResyncer/README.md
+++ SmiResyncer/README.md
@@ -6,4 +6,6 @@ Resync SAMI file.
 
 ## To Use
 
-1. run `python3 smi_resyncer.py target.smi -4.5`
+1. Install python-magic (ex: `pip install -r requirements.txt`)
+2. Install chardet (ex: `pip install -r requirements.txt`)
+3. run `python3 smi_resyncer.py target.smi -4.5` (number means seconds)
--- SmiResyncer/smi_resyncer.py
+++ SmiResyncer/smi_resyncer.py
@@ -2,6 +2,7 @@ import os
 import sys
 import codecs
 import magic
+import chardet
 
 if len(sys.argv) < 3:
     print('Need argument of base path. quitting.')
@@ -11,7 +12,9 @@ file_path = os.path.abspath(sys.argv[1])
 milsec = int(float(sys.argv[2]) * 1000)
 ignore = 'ignore'
 f = magic.Magic(uncompress = True, mime_encoding = True)
-encoding = f.from_file(file_path)
+with open(file_path, mode='rb') as file:
+    binary = file.read()
+    encoding = chardet.detect(binary)['encoding']
 with codecs.open(file_path, 'r', encoding = encoding, errors = ignore) as file:
     lines = file.readlines()
 with codecs.open(file_path, 'w', encoding = encoding, errors = ignore) as file:
