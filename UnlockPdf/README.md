a0101450c2f3249c8b618b928783d33350838a97
retheviper <retheviper@gmail.com>
2022-04-23 14:34:42 +0900

    add unlock pdf

---
+++ UnlockPdf/README.md
@@ -0,0 +1,10 @@
+# Unlock PDF
+
+## TL;DR
+
+Remove password from PDFs in given path.
+
+## To Use
+
+1. Install pikepdf (ex: `pip install -r requirements.txt`)
+1. Run `python3 unlock_pdf.py /dir password`
---
+++ UnlockPdf/unlock_pdf.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import warnings
+from pikepdf import PasswordError, Pdf
+
+if len(sys.argv) < 3:
+    print('- Need argument of base path and password. quitting.')
+    sys.exit(1)  
+
+walk_dir = sys.argv[1]
+password = sys.argv[2]
+
+warnings.simplefilter('error', UserWarning)
+
+for root, _, files in os.walk(walk_dir):
+    for filename in files:
+        if '.pdf' in filename:
+            target = os.path.join(root, filename)
+
+            try:
+                pdf = Pdf.open(target, password=password)
+                if arrive := os.path.join(root, 'unlocked_' + filename):
+                    continue
+                pdf_unlock = Pdf.new()
+                pdf_unlock.pages.extend(pdf.pages)
+                pdf_unlock.save(arrive)
+                print('- unlocked \'{}\'.'.format(filename))
+            except PasswordError:
+                print('- given password not match for \'{}\'.'.format(filename))
+                continue
+            except SystemError:
+                continue
