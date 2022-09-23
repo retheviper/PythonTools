2361104231182d7fe76da96fcc8e9f07a2f51f32
retheviper <retheviper@gmail.com>
2022-09-23 12:00:06 +0900

    fix

--- UnlockPdf/unlock_pdf.py
+++ UnlockPdf/unlock_pdf.py
@@ -5,7 +5,7 @@ from pikepdf import PasswordError, Pdf
 
 if len(sys.argv) < 3:
     print('- Need argument of base path and password. quitting.')
-    sys.exit(1)  
+    sys.exit(1)
 
 walk_dir = sys.argv[1]
 password = sys.argv[2]
@@ -18,16 +18,19 @@ for root, _, files in os.walk(walk_dir):
             target = os.path.join(root, filename)
 
             try:
-                pdf = Pdf.open(target, password=password)
-                arrive = os.path.join(root, 'unlocked_' + filename)
-                if os.path.exists(arrive):
+                pdf = Pdf.open(target)
+            except:
+                try:
+                    pdf = Pdf.open(target, password=password)
+                    arrive = os.path.join(root, 'unlocked_' + filename)
+                    if os.path.exists(arrive):
+                        continue
+                    pdf_unlock = Pdf.new()
+                    pdf_unlock.pages.extend(pdf.pages)
+                    pdf_unlock.save(arrive)
+                    print('- unlocked \'{}\'.'.format(filename))
+                except PasswordError:
+                    print('- given password not match for \'{}\'.'.format(filename))
                     continue
-                pdf_unlock = Pdf.new()
-                pdf_unlock.pages.extend(pdf.pages)
-                pdf_unlock.save(arrive)
-                print('- unlocked \'{}\'.'.format(filename))
-            except PasswordError:
-                print('- given password not match for \'{}\'.'.format(filename))
-                continue
-            except SystemError:
-                continue
+                except SystemError:
+                    continue
